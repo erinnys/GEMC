e3f14f9eea0ac365d05317f81506b8ff3cf951a9
erinnys <riunelt153@gmail.com>
2023-11-20 18:05:27 +0800

    1120-2

--- try1_periodic.py
+++ try1_periodic.py
@@ -36,9 +36,9 @@ class model:
         self.b=b
         self.config=config
         self.tot=[0 for i in range(len(self.latvec))]
-        for i in range(len(num)):
-            for j in range(len(num[i])):
-                self.tot[i]+=num[i][j]
+        for i in range(len(self.num)):
+            for j in range(len(self.num[i])):
+                self.tot[i]+=self.num[i][j]
         self.lab=lab
         self.perio='off'
         self.sigma=sigma
@@ -97,12 +97,28 @@ class model:
                             for z in range(len(self.config[index2][k].T)):
                                 if i==k and j==z:
                                     pass
+                                elif self.dist(index2,i,j,k,z)>cutoff:
+                                    pass
                                 else:
                                     utot=utot+4*((epsilon[i]*epsilon[k])**0.5)*((((self.sigma[i]+self.sigma[k])/2)/self.dist(index2,i,j,k,z))**12-(((self.sigma[i]+self.sigma[k])/2)/self.dist(i,j,k,z))**6)
                 return utot/2 
         else:
             if index==1:
                 utot=0
+                for i in range(len(self.config[index2])):
+                    for j in range(len(self.config[index2][i].T)):
+                        for k in range(len(self.config[index2])):
+                            for z in range(len(self.config[index2][k].T)):
+                                for a in range(3):
+                                    for b in range(3):
+                                        for c in range(3):
+                                            if i==k and j==z and a==b==c==0:
+                                                pass
+                                            elif round(np.linalg.norm(self.config[index2][i].T[j]-self.config[index2][k].T[z])+np.array([a,b,c])*self.latvec,5)>cutoff:
+                                                pass
+                                            else:
+                                                utot=utot+4*((epsilon[i]*epsilon[k])**0.5)*((((self.sigma[i]+self.sigma[k])/2)/round(np.linalg.norm(self.config[index2][i].T[j]-self.config[index2][k].T[z])+np.array([a,b,c])*self.latvec,5))**12-(((self.sigma[i]+self.sigma[k])/2)/round(np.linalg.norm(self.config[index2][i].T[j]-self.config[index2][k].T[z])+np.array([a,b,c])*self.latvec,5))**6)
+                return utot/2 
             return 0
     #計算config能量
     # 1 : Lennard-Jones
@@ -121,27 +137,56 @@ class model:
                 self.config[1]=self.config[1]/(1-cho)
     
     def parexchange(self):
-        
+        cho=np.random.choice([0,1])
+        cho1=np.random.randint(0,len(self.config[cho]))
+        cho2=np.random.randint(0,len(self.config[cho][cho1].T))
+        temp1=self.config[0]
+        temp2=self.config[1]
+        oldE=self.configE(1,0)+self.configE(1,1)
+        temp3=self.config[cho][cho1].T
+        temp4=np.delete(temp3,[cho2],0)
+        temp5=np.append(temp3[cho2],self.config[abs(1-cho)][cho1].T)
+        self.config[cho][cho1]=temp4.T
+        self.config[abs(1-cho)][cho1]=temp5.T
+        if np.exp(-self.b*(self.configE(1,0)+(self.configE(1,1))-oldE))*self.tot[cho]*self.latvec[abs(1-cho)][0]**3/((self.latvec[cho][0]**3)*(self.tot[abs(1-cho)]+1))>1:
+            self.tot[cho]=self.tot[cho]-1
+            self.tot[abs(1-cho)]=self.tot[abs(1-cho)]+1
+            self.num[cho][cho1]=self.num[cho][cho1]-1
+            self.num[abs(1-cho)][cho1]=self.num[abs(1-cho)][cho1]+1
+        else:
+            if np.random.random()<np.exp(-self.b*(self.configE(1,0)+(self.configE(1,1))-oldE))*self.tot[cho]*self.latvec[abs(1-cho)][0]**3/((self.latvec[cho][0]**3)*(self.tot[abs(1-cho)]+1)):
+                self.tot[cho]=self.tot[cho]-1
+                self.tot[abs(1-cho)]=self.tot[abs(1-cho)]+1
+                self.num[cho][cho1]=self.num[cho][cho1]-1
+                self.num[abs(1-cho)][cho1]=self.num[abs(1-cho)][cho1]+1
+            else:
+                self.config[0]=temp1
+                self.config[1]=temp2
         return 0
     
     
     def ranchoiceandmove(self,steplength):
         cho=np.random.choice([0,1])
         cho0=np.random.choice([1,-1])
-        cho1=np.random.randint(0,len(self.config))
-        cho2=np.random.randint(0,len(self.config[cho1].T))
-        cho3=np.random.randint(0,len(self.latvec))
+        cho1=np.random.randint(0,len(self.config[cho]))
+        cho2=np.random.randint(0,len(self.config[cho][cho1].T))
+        cho3=np.random.randint(0,len(self.latvec[cho]))
         cho4=steplength*round(np.random.rand(),5)
         oldE=self.configE(1,cho)
-        self.config[cho][cho1][cho3,cho2]=self.config[cho][cho1][cho3,cho2]+(cho0*cho4*self.latvec[[cho]][cho3])/100
+        self.config[cho][cho1][cho3,cho2]=self.config[cho][cho1][cho3,cho2]+(cho0*cho4*self.latve[cho][cho3])/100
         if np.exp(-self.b*(self.configE(1,cho)-oldE))>=1:
             pass
         else:
             if np.random.random()<np.exp(-self.b*(self.configE(1,cho)-oldE)):
                 pass
             else:
-                self.config[cho1][cho3,cho2]=self.config[cho1][cho3,cho2]-(cho0*cho4*self.latvec[cho3])/100
-
+                self.config[cho1][cho3,cho2]=self.config[cho1][cho3,cho2]-(cho0*cho4*self.latvec[cho][cho3])/100
+    
+    
+    
+    def density(self,ind):
+        return self.tot[ind]/(self.latvec[ind][0]**3)
+    
     #隨機取樣與移動
     def rdf(self,lim):
         
